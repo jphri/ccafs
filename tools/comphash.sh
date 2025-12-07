87c22ed9121f4bea436c9f058860412df47d68ad
João Pedro Hrihorowitsch Silva <jp.hri.silva@gmail.com>
2025-12-07 00:12:27 -0300

    increased compilation speed by not preprocessing the file

--- tools/comphash.sh
+++ tools/comphash.sh
@@ -4,9 +4,14 @@ FILE=$1
 COMPILER_FLAGS=$(pwd)/compiler-flags
 COMPILER=$(pwd)/compiler
 
+get-deps()
+{
+	cat $(gcc $(cat compiler-flags) -MM $FILE | sed -e 's/.*: //' -e 's/\\$//' -e 's/^ //' | tr ' ' '\n' | sed '/^$/d' | sort) | b3sum | cut -d' ' -f1
+}
+
 get-hash() 
 {
-	cpp `cat $COMPILER_FLAGS` $FILE | cat - $COMPILER_FLAGS | (cat - && `cat $COMPILER` -v 2>&1) | b3sum - | cut -f1 -d' '
+	(cat $FILE $COMPILER_FLAGS $COMPILER | b3sum | cut -d' ' -f1 && get-deps) | b3sum | cut -d' ' -f1
 }
 
 echo $(pwd)/.cache/`get-hash`
